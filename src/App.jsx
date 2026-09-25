714847011e23c2d22e78c51f30f1c6b4f931a73c
kunthananan <kunthananan.n@ku.th>
2026-09-25 14:33:50 +0700

    อัปเดตโค้ดใหม่

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+// Components
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
