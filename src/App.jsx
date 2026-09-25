028d64ed1b1830b3608eced96dea88436676ffa8
kunthananan <kunthananan.n@ku.th>
2026-09-25 14:38:53 +0700

    อัปเดตโค้ดใหม่

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
 
 // Components
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Number from "./components/Number/Number";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Skills />
+      <Portfolio />
+      <Number />
+      <Footer />
     </>
   );
 }
